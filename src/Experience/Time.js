bdf290355af013795dc73013418ee3d2b31af6e1
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-04 00:08:43 -0500

    Add tick method

--- src/Experience/Time.js
+++ src/Experience/Time.js
@@ -11,5 +11,20 @@ export default class Time extends EventEmitter {
     // Delta time - the time between the current frame and the previous frame
     // We set it as 16 by default because that is close to how many milliseconds there is between two frames at 60fps
     this.delta = 16;
+
+    window.requestAnimationFrame(() => {
+      this.tick();
+    });
+  }
+
+  tick() {
+    const currentTime = Date.now();
+    this.delta = currentTime - this.current;
+    this.current = currentTime;
+    this.elapsed = this.current - this.start;
+
+    window.requestAnimationFrame(() => {
+      this.tick();
+    });
   }
 }
