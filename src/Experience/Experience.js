fb35386a2f00b50b3f0a3ade5ad174a90cc00ab2
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-03 23:45:53 -0500

    Add Sizes class

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,3 +1,5 @@
+import Sizes from './Utils/Sizes';
+
 export default class Experience {
   constructor(canvas) {
     // Global access
@@ -7,5 +9,7 @@ export default class Experience {
     // Options
     this.canvas = canvas;
 
+    // Setup
+    this.sizes = new Sizes();
   }
 }
