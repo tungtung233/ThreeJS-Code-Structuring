91ba2457caa6903ef6ea0e3f6765715e946bfd2b
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-04 15:55:03 -0500

    Create the camera

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -1,7 +1,25 @@
+import * as THREE from 'three';
 import Experience from './Experience';
 
 export default class Camera {
   constructor() {
     this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+
+    this.setInstance();
+  }
+
+  setInstance() {
+    this.instance = new THREE.PerspectiveCamera(
+      35,
+      this.sizes.width,
+      this.sizes.height,
+      0.1,
+      100
+    );
+
+    this.instance.position.set(6, 4, 8);
+    this.scene.add(this.instance);
   }
 }
