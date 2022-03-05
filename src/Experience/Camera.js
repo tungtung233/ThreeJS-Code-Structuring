fbd21ddfad997fbb9218c5578474b229a5c4c0d5
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-05 14:51:09 -0500

    Fix bug in perspective camera setInstance method

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -16,8 +16,7 @@ export default class Camera {
   setInstance() {
     this.instance = new THREE.PerspectiveCamera(
       35,
-      this.sizes.width,
-      this.sizes.height,
+      this.sizes.width / this.sizes.height,
       0.1,
       100
     );
