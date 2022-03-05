efe8dded0911283f4982cf875e5dba564572a266
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-05 14:57:23 -0500

    Instantiate Environment class

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,8 +1,20 @@
+import * as THREE from 'three';
 import Experience from '../Experience';
+import Environment from './Environment';
 
 export default class World {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
+
+    // Test mesh - here so we can make sure the Environment stuff is working
+    const testMesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshStandardMaterial()
+    );
+    this.scene.add(testMesh);
+
+    // Setup
+    this.environment = new Environment();
   }
 }
