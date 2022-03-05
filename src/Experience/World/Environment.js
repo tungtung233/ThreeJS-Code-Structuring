7b36e5358006e6819f05f55a7091bb0ffe6a8a70
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-05 16:21:53 -0500

    update all mesh materials in the scene with the env map

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -23,10 +23,25 @@ export default class Environment {
 
   setEnvironmentMap() {
     this.environmentMap = {};
-    this.environmentMap.intensite = 0.4;
+    this.environmentMap.intensity = 0.4;
     this.environmentMap.texture = this.resources.items.environmentMapTexture;
     this.environmentMap.texture.encoding = THREE.sRGBEncoding;
 
     this.scene.environment = this.environmentMap.texture;
+
+    this.environmentMap.updateMaterial = () => {
+      this.scene.traverse((child) => {
+        if (
+          child instanceof THREE.Mesh &&
+          child.material instanceof THREE.MeshStandardMaterial
+        ) {
+          child.material.envMap = this.environmentMap.texture;
+          child.material.envMapIntensity = this.environmentMap.intensity;
+          child.material.needsUpdate = true;
+        }
+      });
+    };
+
+    this.environmentMap.updateMaterial();
   }
 }
