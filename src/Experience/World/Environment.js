c11d50911c6d3f7895eb30f45b864cf2ef57c4cc
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-05 15:00:14 -0500

    Create sunlight

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -1,8 +1,21 @@
+import * as THREE from 'three';
 import Experience from '../Experience';
 
 export default class Environment {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
+
+    this.setSunlight();
+  }
+
+  setSunlight() {
+    this.sunlight = new THREE.DirectionalLight('#ffffff', 4);
+    this.sunlight.castShadow = true;
+    this.sunlight.shadow.camera.far = 15;
+    this.sunlight.shadow.mapSize.set(1024, 1024);
+    this.sunlight.shadow.normalizeBias = 0.05;
+    this.sunlight.position.set(3, 3, -2.25);
+    this.scene.add(this.sunlight);
   }
 }
