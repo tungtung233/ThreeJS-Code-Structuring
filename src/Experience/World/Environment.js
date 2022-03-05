8455f2fc82cb23e4c100ce26b9ce941948954c6e
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-05 16:16:53 -0500

    Add Environment Map to the scene

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -5,8 +5,10 @@ export default class Environment {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
 
     this.setSunlight();
+    this.setEnvironmentMap();
   }
 
   setSunlight() {
@@ -17,5 +19,14 @@ export default class Environment {
     this.sunlight.shadow.normalizeBias = 0.05;
     this.sunlight.position.set(3, 3, -2.25);
     this.scene.add(this.sunlight);
+  }
+
+  setEnvironmentMap() {
+    this.environmentMap = {};
+    this.environmentMap.intensite = 0.4;
+    this.environmentMap.texture = this.resources.items.environmentMapTexture;
+    this.environmentMap.texture.encoding = THREE.sRGBEncoding;
+
+    this.scene.environment = this.environmentMap.texture;
   }
 }
