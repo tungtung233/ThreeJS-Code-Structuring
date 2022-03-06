00a18a091c418e37510f7a711038f7861445ecab
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-06 15:20:44 -0500

    Fix typo when bringing in gltfLoader

--- src/Experience/Utils/Resources.js
+++ src/Experience/Utils/Resources.js
@@ -21,7 +21,7 @@ export default class Resources extends EventEmitter {
 
   setLoaders() {
     this.loaders = {};
-    this.loaders.gltf = new GLTFLoader();
+    this.loaders.gltfLoader = new GLTFLoader();
     this.loaders.textureLoader = new THREE.TextureLoader();
     this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader();
   }
