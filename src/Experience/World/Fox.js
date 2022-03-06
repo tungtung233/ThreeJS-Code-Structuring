cec40bb8d13d6618e30219edeb2889719d517afe
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-06 15:26:24 -0500

    Enable fox to cast shadows

--- src/Experience/World/Fox.js
+++ src/Experience/World/Fox.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import Experience from '../Experience';
 
 export default class Fox {
@@ -5,5 +6,22 @@ export default class Fox {
     this.experience = new Experience();
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
+
+    // Setup
+    this.resource = this.resources.items.foxModel;
+
+    this.setModel();
+  }
+
+  setModel() {
+    this.model = this.resource.scene;
+    this.model.scale.set(0.02, 0.02, 0.02);
+    this.scene.add(this.model);
+
+    this.model.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.castShadow = true;
+      }
+    });
   }
 }
