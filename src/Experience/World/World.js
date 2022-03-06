44f01174ccb0c941df2af31d36adc338864d93b9
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-06 15:20:56 -0500

    Instantiate Fox class

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three';
 import Experience from '../Experience';
 import Environment from './Environment';
 import Floor from './Floor';
+import Fox from './Fox';
 
 export default class World {
   constructor() {
@@ -9,17 +9,11 @@ export default class World {
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
 
-    // Test mesh - here so we can make sure the Environment stuff is working
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial()
-    );
-    this.scene.add(testMesh);
-
     // Wait for resources
     this.resources.on('ready', () => {
       // Setup
       this.floor = new Floor();
+      this.fox = new Fox();
       this.environment = new Environment(); // envMap has to be after all the other materials, since it will only update what has already been instantiated
     });
   }
