4cc57ae4065849ff6382694d9011a272cd09830e
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-08 16:51:14 -0500

    create accessible gui controls from #debug

--- src/Experience/Utils/Debug.js
+++ src/Experience/Utils/Debug.js
@@ -1,5 +1,11 @@
+import * as dat from 'lil-gui';
+
 export default class Debug {
   constructor() {
-    console.log('debug started')
+    this.active = window.location.hash === '#debug';
+
+    if (this.active) {
+      this.ui = new dat.GUI();
+    }
   }
 }
