c3bd1488ee9cbe94f42198736d0dfccc6cb7c950
Veronica Leung <tungtungleung233@hotmail.com>
2022-03-08 17:08:28 -0500

    Add a mixer for smooth transitions between fox animations

--- src/Experience/World/Fox.js
+++ src/Experience/World/Fox.js
@@ -10,8 +10,8 @@ export default class Fox {
     this.debug = this.experience.debug;
 
     // Debug
-    if(this.debug.active) {
-      this.debugFolder = this.debug.ui.addFolder('fox')
+    if (this.debug.active) {
+      this.debugFolder = this.debug.ui.addFolder('fox');
     }
 
     // Setup
@@ -36,10 +36,33 @@ export default class Fox {
   setAnimation() {
     this.animation = {};
     this.animation.mixer = new THREE.AnimationMixer(this.model);
-    this.animation.action = this.animation.mixer.clipAction(
+
+    this.animation.actions = {};
+
+    this.animation.actions.idle = this.animation.mixer.clipAction(
       this.resource.animations[0]
     );
-    this.animation.action.play();
+    this.animation.actions.walking = this.animation.mixer.clipAction(
+      this.resource.animations[1]
+    );
+    this.animation.actions.running = this.animation.mixer.clipAction(
+      this.resource.animations[2]
+    );
+
+    this.animation.actions.current = this.animation.actions.idle;
+    this.animation.actions.current.play();
+
+    this.animation.play = (name) => {
+      const oldAction = this.animation.actions.current;
+      const newAction = this.animation.actions[name];
+
+      newAction.reset();
+      newAction.play();
+      // blend the two animations together for a smooth transition, over the period of 1 second
+      newAction.crossFadeFrom(oldAction, 1);
+
+      this.animation.actions.current = newAction;
+    };
   }
 
   update() {
